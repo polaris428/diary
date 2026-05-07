256532024e9482a05cb7541d1e281d09f5f14191
Jeongin205 <ji801089@gmail.com>
2026-05-07 23:38:22 +0900

    :bug: [Fix] 빌드 시 발생하는 타입 에러 수정 및 레거시 코드 삭제

--- components/base/BaseBadge.vue
+++ components/base/BaseBadge.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { DiaryMood } from '~/types';
 const props = defineProps<{
   mood: DiaryMood | null;
 }>();
--- features/diary-list/components/DiaryCard.vue
+++ features/diary-list/components/DiaryCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { DiaryEntry } from '~/types';
 defineProps<{
   entry: DiaryEntry;
 }>();
